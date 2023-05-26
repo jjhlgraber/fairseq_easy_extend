0845ab323dcdf9ab535122ac38cbc72cb1a01098
Job <jjhlgraber@gmail.com>
2023-05-26 18:23:14 +0200

    Started on _compute_loss

--- fairseq_easy_extend/criterions/rl_criterion.py
+++ fairseq_easy_extend/criterions/rl_criterion.py
@@ -10,11 +10,16 @@ from dataclasses import dataclass, field
 
 from fairseq.logging import metrics
 
+# Added imports
+import torch.nn.functional as F
+from torchtext.data.metrics import bleu_score
+
 
 @dataclass
 class RLCriterionConfig(FairseqDataclass):
-    sentence_level_metric: str = field(default="bleu",
-                                       metadata={"help": "sentence level metric"})
+    sentence_level_metric: str = field(
+        default="bleu", metadata={"help": "sentence level metric"}
+    )
 
 
 @register_criterion("rl_loss", dataclass=RLCriterionConfig)
@@ -22,9 +27,7 @@ class RLCriterion(FairseqCriterion):
     def __init__(self, task, sentence_level_metric):
         super().__init__(task)
         self.metric = sentence_level_metric
-        self.tokenizer = encoders.build_tokenizer(Namespace(
-            tokenizer='moses'
-        ))
+        self.tokenizer = encoders.build_tokenizer(Namespace(tokenizer="moses"))
         self.tgt_dict = task.target_dictionary
 
     def forward(self, model, sample, reduce=True):
@@ -44,7 +47,7 @@ class RLCriterion(FairseqCriterion):
         tgt_tokens, prev_output_tokens = sample["target"], sample["prev_target"]
 
         outputs = model(src_tokens, src_lengths, prev_output_tokens, tgt_tokens)
-        #get loss only on tokens, not on lengths
+        # get loss only on tokens, not on lengths
         outs = outputs["word_ins"].get("out", None)
         masks = outputs["word_ins"].get("mask", None)
 
@@ -60,7 +63,7 @@ class RLCriterion(FairseqCriterion):
             "ntokens": ntokens,
             "nsentences": nsentences,
             "sample_size": sample_size,
-            "reward": reward.detach()
+            "reward": reward.detach(),
         }
 
         return loss, sample_size, logging_output
@@ -75,9 +78,7 @@ class RLCriterion(FairseqCriterion):
                 # BLEU scores. Instead, we use a somewhat more verbose
                 # alternative that is unlikely to appear in the real
                 # reference, but doesn't get split into multiple tokens.
-                unk_string=(
-                    "UNKNOWNTOKENINREF" if escape_unk else "UNKNOWNTOKENINHYP"
-                ),
+                unk_string=("UNKNOWNTOKENINREF" if escape_unk else "UNKNOWNTOKENINHYP"),
             )
             s = self.tokenizer.decode(s)
         return s
@@ -102,68 +103,72 @@ class RLCriterion(FairseqCriterion):
         masks:   batch x len
         """
 
-        #padding mask
+        # padding mask
         ##If you take mask before you do sampling: you sample over a BATCH and your reward is on token level
-        #if you take mask after, you sample SENTENCES and calculate reward on a sentence level 
-        #but make sure you apply padding mask after both on log prob outputs, reward and id's (you might need them for gather function to           extract log_probs of the samples)
+        # if you take mask after, you sample SENTENCES and calculate reward on a sentence level
+        # but make sure you apply padding mask after both on log prob outputs, reward and id's (you might need them for gather function to           extract log_probs of the samples)
 
-        #Example 1: mask before sampling
-        #if masks is not None:
-        #    outputs, targets = outputs[masks], targets[masks]
+        # Example 1: mask before sampling
+        if masks is not None:
+            outputs, targets = outputs[masks], targets[masks]
 
-        #we take a softmax over outputs
-        #argmax over the softmax \ sampling (e.g. multinomial)
-        #sampled_sentence = [4, 17, 18, 19, 20]
-        #sampled_sentence_string = tgt_dict.string([4, 17, 18, 19, 20])
-        #target_sentence = "I am a sentence"
-        #with torch.no_grad()
-            #R(*) = eval_metric(sampled_sentence_string, target_sentence)
-            #R(*) is a number, BLEU, сhrf, etc.
+            # we take a softmax over outputs
+            probs = F.softmax(outputs, dim=-1)
+            # argmax over the softmax \ sampling (e.g. multinomial)
+            sampled_sentence_idx = torch.multinomial(probs, 1, replacement=True)
+            sampled_sentence_string = self.tgt_dict.string(sampled_sentence_idx)
 
-        #loss = -log_prob(outputs)*R()
-        #loss = loss.mean()
-        
-        #Example 2: mask after sampling
-        #bsz = outputs.size(0)
-        #seq_len = outputs.size(1)
-        #vocab_size = outputs.size(2)
-        #with torch.no_grad():
-        #probs = F.softmax(outputs, dim=-1).view(-1, vocab_size)
-        #sample_idx  = torch.multinomial(probs, 1,replacement=True).view(bsz, seq_len)
-        #print(sample_idx.shape)
-        #self.tgt_dict = task.tgt_dict in __init__()
-        #sampled_sentence_string = self.tgt_dict.string(sample_idx) #here you might also want to remove tokenization and bpe
-        #print(sampled_sentence_string) --> if you apply mask before, you get a sentence which is one token 
-        #imagine output[mask]=[MxV] where M is a sequence of all tokens in batch excluding padding symbols
-        #now you sample 1 vocabulary index for each token, so you end up in [Mx1] matrix
-        #when you apply string, it treats every token as a separate sentence --> hence you calc token-level metric. SO it makes much more sense to apply mask after sampling(!)
+            with torch.no_grad():
+                if self.metric.lower() == "bleu":
+                    reward_vals = bleu_score(sampled_sentence_string, targets)
+                else:
+                    raise Exception("Not yet implemented")
 
-        ####HERE calculate metric###
-        #with torch.no_grad()
-        #reward = eval_metric(sampled_sentence_string, target_sentence)
-        #reward is a number, BLEU, сhrf, etc.
-        #expand it to make it of a shape BxT - each token gets the same reward value (e.g. bleu is 20, so each token gets reward of 20 [20,20,20,20,20])
-    
-        #now you need to apply mask on both outputs and reward
-        #if masks is not None:
-        #    outputs, targets = outputs[masks], targets[masks]
-        #    reward, sample_idx = reward[mask], sample_idx[mask]
-        #log_probs = F.log_probs(outputs, dim=-1)
-        #log_probs_of_samples = torch.gather(...)
-        #loss = -log_probs*reward
-        # loss = loss.mean()
-        
-        #For more about mask see notes on NLP2-notes-on-mask
+                loss = -torch.log(probs) * reward_vals
+                loss = loss.mean()
+        # Example 2: mask after sampling
+        else:
+            bsz, seq_len, vocab_size = outputs.size()
+            with torch.no_grad():
+                probs = F.softmax(outputs, dim=-1).view(-1, vocab_size)
+                sample_idx = torch.multinomial(probs, 1, replacement=True).view(
+                    bsz, seq_len
+                )
+                # print(sample_idx.shape)
+                sampled_sentence_string = self.tgt_dict.string(
+                    sample_idx
+                )  # here you might also want to remove tokenization and bpe
+            # print(sampled_sentence_string) --> if you apply mask before, you get a sentence which is one token
+            # imagine output[mask]=[MxV] where M is a sequence of all tokens in batch excluding padding symbols
+            # now you sample 1 vocabulary index for each token, so you end up in [Mx1] matrix
+            # when you apply string, it treats every token as a separate sentence --> hence you calc token-level metric. SO it makes much more sense to apply mask after sampling(!)
 
+            ####HERE calculate metric###
+            with torch.no_grad():
+                if self.metric.lower() == "bleu":
+                    reward_vals = bleu_score(sampled_sentence_string, targets)
+                else:
+                    raise Exception("Not yet implemented")
+            # expand it to make it of a shape BxT - each token gets the same reward value (e.g. bleu is 20, so each token gets reward of 20 [20,20,20,20,20])
 
+            # now you need to apply mask on both outputs and reward
+            if masks is not None:
+                outputs, targets = outputs[masks], targets[masks]
+                reward, sample_idx = reward[masks], sample_idx[masks]
+            log_probs = F.log_probs(outputs, dim=-1)
+            # log_probs_of_samples = torch.gather(...)
+            loss = -log_probs * reward
+            loss = loss.mean()
 
-        return loss, reward.mean()
+            # For more about mask see notes on NLP2-notes-on-mask
+
+        return loss, reward_vals.mean()
 
     @staticmethod
     def reduce_metrics(logging_outputs) -> None:
         """Aggregate logging outputs from data parallel training."""
         loss_sum = sum(log.get("loss", 0) for log in logging_outputs)
-        reward_sum= sum(log.get("reward", 0) for log in logging_outputs)
+        reward_sum = sum(log.get("reward", 0) for log in logging_outputs)
         ntokens = sum(log.get("ntokens", 0) for log in logging_outputs)
         sample_size = sum(log.get("sample_size", 0) for log in logging_outputs)
 
